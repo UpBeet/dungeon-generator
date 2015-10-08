3ace8d7dd7c698e5104d5c8c8b83bb7fff7320bf
Weslo <wesley.rockholz@gmail.com>
2015-10-07 20:55:07 -0400

    establish board and tile logical definitions

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Board definition, logic passed between client and server.
+/// </summary>
+public class BoardDefinition {
+
+	/// <summary>
+	/// The width of the board.
+	/// </summary>
+	public int Width { get; private set; }
+
+	/// <summary>
+	/// The height of the board.
+	/// </summary>
+	public int Height { get; private set; }
+
+	/// <summary>
+	/// Array of tile definitions.
+	/// </summary>
+	private Tile[] tiles;
+
+	/// <summary>
+	/// Instantiates a new BoardDefinition object.
+	/// </summary>
+	/// <param name="width">Width of the board.</param>
+	/// <param name="height">Height of the board.</param>
+	public BoardDefinition (int width, int height) {
+
+		// Copy dimensions.
+		this.Width = width;
+		this.Height = height;
+
+		// Allocate memory for the tiles.
+		tiles = new Tile[width * height];
+
+		// Initialize each tile.
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				tiles [y * width + x] = new Tile (x, y);
+			}
+		}
+	}
+}
+
+/// <summary>
+/// Board component. Exists only on the client, to interface between
+/// the Unity scene and the server's logical definition.
+/// </summary>
+public class BoardController : MonoBehaviour {
+
+	/// <summary>
+	/// Reference to the definition for this board.
+	/// </summary>
+	private BoardDefinition definition;
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Tile definition, logic passed between client and server.
+/// </summary>
+public class Tile {
+
+	/// <summary>
+	/// The X coordinate.
+	/// </summary>
+	public int X { get; private set; }
+
+	/// <summary>
+	/// The Y coordinate.
+	/// </summary>
+	public int Y { get; private set; }
+
+	/// <summary>
+	/// Instantiates a new Tile object.
+	/// </summary>
+	/// <param name="x">The X coordinate of the tile.</param>
+	/// <param name="y">The Y coordinate of the tile.</param>
+	public Tile (int x, int y) {
+		this.X = x;
+		this.Y = y;
+	}
+}
+
+/// <summary>
+/// Tile component. Exists only on the client, to interface between
+/// the Unity scene and the server's logical definition.
+/// </summary>
+public class TileController : MonoBehaviour {
+
+	/// <summary>
+	/// The X coordinate.
+	/// </summary>
+	public int X { get; private set; }
+
+	/// <summary>
+	/// The Y coordinate.
+	/// </summary>
+	/// <value>The y.</value>
+	public int Y { get; private set; }
+
+	/// <summary>
+	/// Reference to the definition for this tile.
+	/// </summary>
+	private Tile tile;
+}
